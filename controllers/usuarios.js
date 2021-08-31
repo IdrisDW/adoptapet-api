1995c1532af2fdf99ed0095b60afad59b66e9523
Juliet <juguquro@gmail.com>
2021-08-30 18:27:32 -0700

    so far sesion 3

---
+++ controllers/usuarios.js
@@ -0,0 +1,25 @@
+const Usuario = require('../models/Usuario')
+
+//CRUD
+
+function crearUsuario(req,res){
+    var usuario = new Usuario (req.body);
+    res.status(200).send(usuario);
+
+}
+
+function obtenerUsuarios(req,res){
+    var usuario1 = new Usuario(1, 'juancho' , 'Juan', 'Vega', 'juan@vega.com', 'abc' , 'normal');
+    var usuario2 = new Usuario(2, 'montse', 'Montserrat', 'Vega', 'mon@vega.com' , '123', 'anunciante');
+
+    res.send([usuario1,usuario2]);
+
+}
+
+function modificarUsuario(req,res){
+var usuario = new Usuario(req.params.id, 'juancho',  'Juan', 'Vega', 'juan@vega.com', 'abc' , 'normal' )
+var modificaciones = req.body 
+usuario = { ...usuario, ...modificaciones}
+res.send(usuario)
+
+}
